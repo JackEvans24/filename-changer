62b6fa9ba314703a16d6ede069f70511eaee86ad
Jack <jackevans24@googlemail.com>
2020-02-19 15:21:41 +0000

    Add console helpers and file info extensions classes

---
+++ FilenameChanger/Helpers/ConsoleHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FilenameChanger.Helpers
+{
+    public static class ConsoleHelpers
+    {
+        public static bool ConfirmChange(string confirmMessage = "Confirm Change")
+        {
+            Console.WriteLine();
+            Console.WriteLine(confirmMessage + ": [Y] Yes, [N] No");
+            return Console.ReadLine().ToLower() == "y";
+        }
+    }
+}
---
+++ FilenameChanger/Helpers/FileInfoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace FilenameChanger.Helpers
+{
+    public static class FileInfoExtensions
+    {
+        public static string FilenameWithoutExtension(this FileInfo file)
+        {
+            return file.Name.Substring(0, file.Name.LastIndexOf('.'));
+        }
+    }
+}
