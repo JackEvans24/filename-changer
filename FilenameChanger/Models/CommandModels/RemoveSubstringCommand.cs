67960c34661dcf6ec1d61e2a48051942c34eae4d
Jack <jackevans24@googlemail.com>
2020-02-19 15:28:34 +0000

    Extract processing to command class Implement ManyConsole

---
+++ FilenameChanger/Models/CommandModels/RemoveSubstringCommand.cs
@@ -0,0 +1,93 @@
+﻿using FilenameChanger.Helpers;
+using ManyConsole;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FilenameChanger.Models.CommandModels
+{
+    public class RemoveSubstringCommand : ConsoleCommand
+    {
+        public string Pattern { get; set; }
+        public string[] Extensions { get; set; }
+        public string FileLocation { get; set; }
+
+        public RemoveSubstringCommand()
+        {
+            this.SetUpManyConsole();
+
+            if (string.IsNullOrWhiteSpace(this.FileLocation))
+                this.FileLocation = Environment.CurrentDirectory;
+        }
+
+        private void SetUpManyConsole()
+        {
+            IsCommand("remove", "Remove a substring of all filenames in the directory");
+
+            HasLongDescription("This can be used to remove the specified substring from files in the directory.\r\n" +
+                "Useful when a downloaded set of files includes a suffix you don't want.");
+
+            HasRequiredOption("p|pattern=", "The substring to remove from files", p => this.Pattern = p);
+
+            HasOption("e|ext=", "A comma separated list of file types to alter. If not provided, all files are altered",
+                e => this.Extensions = e.Split(','));
+
+            HasOption("f|file=", "The full path of the file. Defaults to the current console location",
+                f => this.FileLocation = f);
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            var files = this.GetValidFiles();
+
+            if (!files.Any())
+                return NoFiles();
+
+            foreach (var file in files)
+            {
+                var filename = file.FilenameWithoutExtension();
+                Console.WriteLine("{0} => {1}", filename, filename.Replace(this.Pattern, string.Empty));
+            }
+
+            if (ConsoleHelpers.ConfirmChange())
+            {
+                foreach (var file in files)
+                {
+                    this.RenameFile(file);
+                }
+            }
+
+            return 0;
+        }
+
+        private IEnumerable<FileInfo> GetValidFiles()
+        {
+            var files = Directory.GetFiles(this.FileLocation).Select(f => new FileInfo(f));
+
+            if (Extensions != null)
+                files = files.Where(f => Extensions.Contains(f.Extension));
+
+            return files;
+        }
+
+        private int NoFiles()
+        {
+            Console.WriteLine($"No files found in directory: {this.FileLocation}");
+            return 1;
+        }
+
+        private void RenameFile(FileInfo file)
+        {
+            if (!file.FilenameWithoutExtension().Contains(this.Pattern))
+                return;
+
+            var newFilename = file.FilenameWithoutExtension().Replace(this.Pattern, string.Empty).Trim() +
+                file.Extension;
+            newFilename = newFilename.Replace("  ", " ");
+
+            File.Move(file.FullName, $"{file.DirectoryName}/{newFilename}");
+            File.Delete(file.FullName);
+        }
+    }
+}
--- FilenameChanger/Program.cs
+++ FilenameChanger/Program.cs
@@ -1,73 +1,21 @@
-﻿using NDesk.Options;
+﻿using ManyConsole;
 using System;
 using System.Collections.Generic;
-using System.IO;
 
 namespace FilenameChanger
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            var showHelp = false;
-            var directory = Environment.CurrentDirectory;
-            var pattern = string.Empty;
+            var commands = GetCommands();
 
-            var p = new OptionSet() {
-                { "d|directory=", "the {DIRECTORY} of files to change.",
-                   v => directory = v },
-                { "p|pattern=", "the string {PATTERN} to remove from file names.",
-                   v => pattern = v },
-                { "h|help",  "show this message and exit",
-                   v => showHelp = v != null },
-            };
+            return ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
+        }
 
-            List<string> extra;
-            try
-            {
-                extra = p.Parse(args);
-            }
-            catch (OptionException e)
-            {
-                Console.Write("greet: ");
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Try `greet --help' for more information.");
-                return;
-            }
-
-            if (string.IsNullOrWhiteSpace(pattern))
-            {
-                Console.WriteLine("Directory or pattern not included");
-                return;
-            }
-
-            Console.WriteLine("File changes:");
-            Console.WriteLine();
-
-            var files = Directory.GetFiles(directory);
-            foreach (var file in files)
-            {
-                var fileObj = new FileInfo(file);
-                var filename = fileObj.Name;
-                Console.WriteLine("{0} => {1}", filename, filename.Replace(pattern, string.Empty));
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Confirm change: [Y] Yes, [N] No");
-            var confirm = Console.ReadLine().ToLower() == "y";
-
-            if (confirm)
-            {
-                foreach (var file in files)
-                {
-                    var fileObj = new FileInfo(file);
-                    if (!fileObj.Name.Contains(pattern))
-                        continue;
-
-                    File.Move(fileObj.FullName, $"{fileObj.DirectoryName}/{fileObj.Name.Replace(pattern, string.Empty).Trim()}");
-                    File.Delete(fileObj.FullName);
-                }
-            }
+        public static IEnumerable<ConsoleCommand> GetCommands()
+        {
+            return ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof(Program));
         }
     }
 }
